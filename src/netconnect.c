06d0e91d774caa6b99df6d2e8f73e0fb42ec6aab
Bernd Wachter <bwachter-oops@lart.info>
2009-11-19 02:23:19 +0100

    indent fixes, added struct for outgoing binding

--- src/netconnect.c
+++ src/netconnect.c
@@ -4,33 +4,43 @@
 
 // opens a network connection and returns a socket descriptor
 int netconnect(char *hostname, char *servicename){
-	struct addrinfo *res;
-	struct addrinfo hints;
-	int sd, err;
+  return netconnect2(hostname, servicename, (char*)NULL);
+}
+
+int netconnect2(char *hostname, char *servicename, char *bindaddress){
+  struct addrinfo *res;
+  struct addrinfo hints;
+  int sd, err;
 #ifdef __WIN32__
-	WSADATA wsaData;
+  WSADATA wsaData;
 #endif
 
-	memset(&hints, 0, sizeof hints);
-	hints.ai_flags = AI_PASSIVE;
-	hints.ai_socktype = SOCK_STREAM;
+  if (bindaddress==NULL)
+    logmsg(L_ERROR, F_NET, "not trying to bind to anything", NULL);
+  else
+    logmsg(L_ERROR, F_NET, "will try binding", NULL);
+
+  memset(&hints, 0, sizeof hints);
+  hints.ai_flags = AI_PASSIVE;
+  hints.ai_socktype = SOCK_STREAM;
 
 #ifdef __WIN32__
-	WSAStartup( 0x0202, &wsaData );
+  WSAStartup( 0x0202, &wsaData );
 #endif
-	if ((err=netaddrinfo(hostname, servicename, &hints, &res))){
-		logmsg(L_ERROR, F_NET, "unable to resolve host", NULL);
-		return -1;
-	} else {
-		while (res){
-			if ((sd=netsocket(&*res)) > 0)
-				return sd;
-			if(res->ai_next==NULL)
-				logmsg(L_INFO, F_NET, "res->ai_next is NULL", NULL);
-			else 
-				logmsg(L_INFO, F_NET, "trying next element", NULL);
-			res=res->ai_next;
-		}
-	}
-	return -1;
+  if ((err=netaddrinfo(hostname, servicename, &hints, &res))){
+    logmsg(L_ERROR, F_NET, "unable to resolve host", NULL);
+    return -1;
+  } else {
+    while (res){
+      //TODO check bind address, match ai_family 
+      if ((sd=netsocket(&*res)) > 0)
+        return sd;
+      if(res->ai_next==NULL)
+        logmsg(L_INFO, F_NET, "res->ai_next is NULL", NULL);
+      else 
+        logmsg(L_INFO, F_NET, "trying next element", NULL);
+      res=res->ai_next;
+    }
+  }
+  return -1;
 }
